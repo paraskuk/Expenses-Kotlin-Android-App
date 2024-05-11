04b0bbca8ce951c6a7d8f086c6f8323650a1d763
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-05-11 19:05:47 +0100

    Adding all core functionality buttons for main screen

--- app/src/main/java/com/paras_test_android/assignment_paras/MainActivity.kt
+++ app/src/main/java/com/paras_test_android/assignment_paras/MainActivity.kt
@@ -6,15 +6,51 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+import android.widget.Button
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity()
+//{
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //this adds the add button
+        val addButton = findViewById<Button>(R.id.addExpensesButton)
+        //and this adds the show button
+        val showButton = findViewById<Button>(R.id.showExpensesButton)
+        //and this adds the edit button for expenses
+        val editButton = findViewById<Button>(R.id.editExpensesButton)
+
+        val statisticsButton = findViewById<Button>(R.id.showStatisticsButton)
+
+        addButton.setOnClickListener {
+            Toast.makeText(this, "Add Expenses", Toast.LENGTH_SHORT).show()
+        }
+
+        showButton.setOnClickListener {
+            Toast.makeText(this, "Show Expenses", Toast.LENGTH_SHORT).show()
+        }
+
+        editButton.setOnClickListener {
+            Toast.makeText(this, "Edit Expenses", Toast.LENGTH_SHORT).show()
+        }
+
+        statisticsButton.setOnClickListener {
+            Toast.makeText(this, "Show Statistics on Expenses", Toast.LENGTH_SHORT).show()
         }
     }
 }
