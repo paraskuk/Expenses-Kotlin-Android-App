d98008b696411ff865b4d74074c42729b09bc233
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-05-11 19:24:23 +0100

    Adding comments

--- app/src/main/java/com/paras_test_android/assignment_paras/MainActivity.kt
+++ app/src/main/java/com/paras_test_android/assignment_paras/MainActivity.kt
@@ -37,18 +37,19 @@ class MainActivity : AppCompatActivity()
 
         val statisticsButton = findViewById<Button>(R.id.showStatisticsButton)
 
+        //add clicklistener for add expenses button
         addButton.setOnClickListener {
             Toast.makeText(this, "Add Expenses", Toast.LENGTH_SHORT).show()
         }
-
+        //add clicklistener for show expenses button
         showButton.setOnClickListener {
             Toast.makeText(this, "Show Expenses", Toast.LENGTH_SHORT).show()
         }
-
+        //add clicklistener for edit expenses button
         editButton.setOnClickListener {
             Toast.makeText(this, "Edit Expenses", Toast.LENGTH_SHORT).show()
         }
-
+        //add clicklistener for statistics  expenses button
         statisticsButton.setOnClickListener {
             Toast.makeText(this, "Show Statistics on Expenses", Toast.LENGTH_SHORT).show()
         }
