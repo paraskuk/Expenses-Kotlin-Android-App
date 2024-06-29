eca62c79703b77ca7de17abe3b6e56f0aa08654e
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-06-29 12:09:01 +0100

    adding comments to EditExpensesAdapter.kt and ExpenseDAO.kt

--- app/src/main/java/com/paras_test_android/assignment_paras/EditExpensesAdapter.kt
+++ app/src/main/java/com/paras_test_android/assignment_paras/EditExpensesAdapter.kt
@@ -9,45 +9,80 @@ import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 
 /***
- * Adapter class for editing expenses
+ * Adapter class for editing expenses this targets the  edit expense screen
  */
 class EditExpensesAdapter(private var expenses: List<ExpenseTable>) : RecyclerView.Adapter<EditExpensesAdapter.ExpenseViewHolder>() {
 
+    /***
+     *
+     * Method to create new views
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExpenseViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_edit_expense, parent, false)
         return ExpenseViewHolder(view)
     }
 
+
+    /***
+     *
+     * Function updating the contents of the itemView  to represent the item.
+     */
     override fun onBindViewHolder(holder: ExpenseViewHolder, position: Int) {
         val expense = expenses[position]
         holder.bind(expense)
     }
 
+    /***
+     *
+     * Function returning the size of the list of expenses
+     */
     override fun getItemCount(): Int {
         return expenses.size
     }
 
+    /***
+     *
+     * Method  to update the list of expenses
+     */
     fun updateExpenses(newExpenses: List<ExpenseTable>) {
         expenses = newExpenses
         notifyDataSetChanged()
     }
 
+    /***
+     *
+     * Function to get all the expenses from db
+     */
     fun getAllExpenses(): List<ExpenseTable> {
         return expenses
     }
 
+    /***
+     *
+     * ExpenseViewHolder class that keeps the edit text fields for the title, amount, date and category
+     * and binds the data to the edit text fields
+     */
     class ExpenseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //private variables for the edit text fields
         private val titleEditText: EditText = itemView.findViewById(R.id.titleEditText)
         private val amountEditText: EditText = itemView.findViewById(R.id.amountEditText)
         private val dateEditText: EditText = itemView.findViewById(R.id.dateEditText)
         private val categoryEditText: EditText = itemView.findViewById(R.id.categoryEditText)
 
+        /***
+         *
+         * Function that binds the data to the edit text fields
+         */
         fun bind(expense: ExpenseTable) {
             titleEditText.setText(expense.title)
             amountEditText.setText(expense.amount.roundToTwoDecimalPlaces().toString())
             dateEditText.setText(expense.date)
             categoryEditText.setText(expense.category)
 
+            /***
+             *
+             * Text listeners for the edit text fields to update the expense object correctly
+             */
             titleEditText.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
--- app/src/main/java/com/paras_test_android/assignment_paras/ExpenseDAO.kt
+++ app/src/main/java/com/paras_test_android/assignment_paras/ExpenseDAO.kt
@@ -8,7 +8,6 @@ import androidx.room.Query
 import androidx.room.Update
 import java.nio.file.Files.delete
 
-//import kotlinx.coroutines.flow.Flow
 
 /**
  * Data Access Object for the expenses table
@@ -16,7 +15,9 @@ import java.nio.file.Files.delete
  */
 @Dao
 interface ExpenseDAO {
-
+    /**
+     * adding a new expense in db
+     */
     @Insert
     suspend fun insertExpense(expense: List<ExpenseTable>)
 
@@ -24,27 +25,50 @@ interface ExpenseDAO {
     @Query("SELECT * FROM expenses")
     suspend fun getAllExpenses(): List<ExpenseTable>
 
+    /**
+     * inserts expenses in the db using list of expenses
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertExpenses(expenses: List<ExpenseTable>)
 
+    /**
+     * updates expenses in the db
+     */
     @Update
     suspend fun updateExpenses(expenses: List<ExpenseTable>)
 
+    /**
+     * deletes expenses  in Room db
+     */
     @Delete
     suspend fun deleteExpenses(expenses: List<ExpenseTable>)
 
+    /**
+     * deletes all expenses in Room db this is a helper function
+     */
     @Query("DELETE FROM expenses")
     suspend fun deleteAllExpenses()
 
+    /**
+     * select statement for getting the most frequent title in show statistics
+     */
     @Query("SELECT title, COUNT(title) as count FROM expenses GROUP BY title ORDER BY count DESC LIMIT 1")
     suspend fun getMostFrequentTitle(): ExpenseTitleFrequency
 
+    /**
+     * select statement in order to show the most frequent category in show statistics menu
+     */
     @Query("SELECT category, COUNT(category) as count FROM expenses GROUP BY category ORDER BY count DESC LIMIT 1")
     suspend fun getMostFrequentCategory(): ExpenseCategoryFrequency
 
+    /**
+     * select statement for creating tghe average amount per category of expenses in statistics
+     */
     @Query("SELECT category, AVG(amount) as averageAmount FROM expenses GROUP BY category")
     suspend fun getAverageAmountPerCategory(): List<CategoryAverageAmount>
-
+    /**
+     * select statement to show the most recent expense by date in the show statistics menu
+     */
     @Query("SELECT * FROM expenses ORDER BY date DESC LIMIT 1")
     suspend fun getMostRecentExpense(): ExpenseTable
 
